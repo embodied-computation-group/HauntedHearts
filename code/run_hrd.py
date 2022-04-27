c225348c8ffcb254ffe1197fb3083870c6f78e87
LegrandNico <nicolas.legrand@cfin.au.dk>
2022-04-27 17:56:40 +0200

    Fix and re-run meta-d. Re-generate the HTML reports.

--- code/run_hrd.py
+++ code/run_hrd.py
@@ -7,15 +7,15 @@ import multiprocessing as mp
 
 def report_HGF(
     subject,
-    reportPath="/home/nicolas/git/CardioceptionHauntedHouse/reports",
+    reportPath="/home/nicolas/git/HauntedHearts/reports",
 ):
 
     pm.execute_notebook(
-        "/home/nicolas/git/CardioceptionHauntedHouse/code/HeartRateDiscrimination.ipynb",
+        "/home/nicolas/git/HauntedHearts/code/HeartRateDiscrimination.ipynb",
         f"{reportPath}/{subject}.ipynb",
-        parameters=dict(subject=subject),
+        parameters=dict(subject=subject, path="/home/nicolas/git/HauntedHearts/"),
     )
-    command = f"jupyter nbconvert --to html {reportPath}/{subject}.ipynb --output {reportPath}/{subject}_.html"
+    command = f"jupyter nbconvert --to html {reportPath}/{subject}.ipynb --output {reportPath}/{subject.zfill(4)}.html"
     os.system(command)
     os.remove(f"{reportPath}/{subject}.ipynb")
 
@@ -23,9 +23,10 @@ def report_HGF(
 if __name__ == "__main__":
 
     path = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
-    subjects = [sub[:-3] for  sub in os.listdir(f"{path}/data/raw/")]
+    subjects = [sub[:-3] for sub in os.listdir(f"{path}/data/raw/")]
+    subjects = [sub for sub in subjects if sub.isdigit()]
 
-    pool = mp.Pool(processes=30)
+    pool = mp.Pool(processes=50)
     pool.map(report_HGF, subjects)
     pool.close()
     pool.join()
